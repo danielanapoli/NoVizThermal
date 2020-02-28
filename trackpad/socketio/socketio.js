24f82effaf9cb2360caee03a93e05fad35a9f29e
Daniela2 <dnapol01@gmail.com>
2020-02-27 22:58:54 -0500

    Working on building form for web assessments

--- trackpad/socketio/socketio.js
+++ trackpad/socketio/socketio.js
@@ -1,60 +1,91 @@
-//BASE CODE SOURCE: https://maker.pro/arduino/projects/learn-how-to-enable-communication-between-an-arduino-and-web-browser
+let http = require('http')
+let socketio = require('socket.io')
+let fs = require('fs')
+let osc = require('osc-min')
+let dgram = require('dgram')
+let url = require('url')
+let remote_osc_ip;
 
+const ROOT_DIR = "socketio"; //dir to serve static files from
 
-var http = require('http'),
-    socketio = require('socket.io'),
-    fs = require('fs'),
-    osc = require('osc-min'),
-    dgram = require('dgram'),
-    remote_osc_ip;
- 
-var http_server = http.createServer(function(req, res) {
- 
-  fs.readFile(__dirname + '/socketio.html', function(err, data) {
- 
-    if(err) {
-      res.writeHead(500);
-      return res.end('Error loading socket.io.html');
+const MIME_TYPES = {
+  css: "text/css",
+  gif: "image/gif",
+  htm: "text/html",
+  html: "text/html",
+  ico: "image/x-icon",
+  jpeg: "image/jpeg",
+  jpg: "image/jpeg",
+  js: "application/javascript",
+  json: "application/json",
+  png: "image/png",
+  svg: "image/svg+xml",
+  txt: "text/plain"
+}
+    
+function get_mime(filename) {
+  for (let ext in MIME_TYPES) { //Use file extension to determine the correct response MIME type
+    if (filename.indexOf(ext, filename.length - ext.length) !== -1) {
+        return MIME_TYPES[ext]
     }
- 
-    res.writeHead(200);
-    res.end(data);
- 
-  });
- 
+  }
+  return MIME_TYPES["txt"]
+}
+
+var http_server = http.createServer(function(req, res) {
+
+  let filePath = "." + req.url;
+  if (filePath == './') filePath = './index.html';
+
+  fs.readFile(filePath, function(err, content){
+    if (err) {
+      console.log("ERROR: " + JSON.stringify(err)); //Report error to console
+      res.writeHead(404); //Respond with not found 404 to client
+      res.end(JSON.stringify(err));
+      return;
+    }
+    //Respond with file contents
+    res.writeHead(200, { "Content-Type": get_mime(filePath) })
+    res.end(content)
+  })
+
+
+
+
+
+
+  // //Handle URL parsing 
+  // let urlObj = url.parse(req.url, true, false)
+  // console.log("\n============================")
+  // console.log("PATHNAME: " + urlObj.pathname)
+  // console.log("REQUEST: " + ROOT_DIR + urlObj.pathname)
+  // console.log("METHOD: " + req.method)
+  // if (req.method === "POST") {
+  //   console.log("POST request not setup")
+  // }
+  // //Handle GET requests (e.g. queries for another page)
+  // if (req.method === "GET"){
+  //   fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data) {
+  //     if (err) {
+  //       console.log("ERROR: " + JSON.stringify(err)) //Report error to console
+  //       res.writeHead(404) //Respond with not found 404 to client
+  //       res.end(JSON.stringify(err))
+  //       return
+  //     }
+  //     //Respond with file contents
+  //     res.writeHead(200, { "Content-Type": get_mime(urlObj.pathname) })
+  //     res.end(data)
+  //   })
+  // }
+
 });
  
-var io = socketio(http_server);
- 
-var udp_server = dgram.createSocket('udp4', function(msg, rinfo) {
- 
-  var osc_message;
-  try {
-    osc_message = osc.fromBuffer(msg);
-  } catch(err) {
-    return console.log('Could not decode OSC message');
-  }
- 
-  if(osc_message.address != '/socketio') {
-    return console.log('Invalid OSC address');
-  }
- 
-  remote_osc_ip = rinfo.address;
- 
-  io.emit('osc', {
-    x: parseInt(osc_message.args[0].value) || 0
-  });
- 
-});
- 
+let io = socketio(http_server);
 io.on('connection', function(socket) {
- 
   socket.on('browser', function(data) {
- 
     if(! remote_osc_ip) {
       return;
     }
- 
     var osc_msg = osc.toBuffer({
       oscType: 'message',
       address: '/socketio',
@@ -67,15 +98,10 @@ io.on('connection', function(socket) {
         value: parseInt(data.y) || 0
       }]
     });
- 
     udp_server.send(osc_msg, 0, osc_msg.length, 9999, remote_osc_ip);
     console.log('Sent OSC message to %s:9999', remote_osc_ip);
- 
   });
- 
 });
  
 http_server.listen(8080);
 console.log('Starting HTTP server on TCP port 8080');
-udp_server.bind(9998);
-console.log('Starting UDP server on UDP port 9998');
