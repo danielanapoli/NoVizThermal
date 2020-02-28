028cf671ebcdfe12bac5ed04b313d41c5565f1ea
Daniela2 <dnapol01@gmail.com>
2020-02-27 23:48:54 -0500

    Refactored socketio.js to express server

--- trackpad/socketio/socketio.js
+++ trackpad/socketio/socketio.js
@@ -1,86 +1,22 @@
-let http = require('http')
-let socketio = require('socket.io')
-let fs = require('fs')
-let osc = require('osc-min')
-let dgram = require('dgram')
-let url = require('url')
+const express = require('express')
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const path = require('path');
+
+//arduino extras
+//https://maker.pro/arduino/projects/learn-how-to-enable-communication-between-an-arduino-and-web-browser
+const osc = require('osc-min');
+const dgram = require('dgram');
 let remote_osc_ip;
+const PORT = 8080;
 
-const ROOT_DIR = "socketio"; //dir to serve static files from
-
-const MIME_TYPES = {
-  css: "text/css",
-  gif: "image/gif",
-  htm: "text/html",
-  html: "text/html",
-  ico: "image/x-icon",
-  jpeg: "image/jpeg",
-  jpg: "image/jpeg",
-  js: "application/javascript",
-  json: "application/json",
-  png: "image/png",
-  svg: "image/svg+xml",
-  txt: "text/plain"
-}
-    
-function get_mime(filename) {
-  for (let ext in MIME_TYPES) { //Use file extension to determine the correct response MIME type
-    if (filename.indexOf(ext, filename.length - ext.length) !== -1) {
-        return MIME_TYPES[ext]
-    }
-  }
-  return MIME_TYPES["txt"]
-}
-
-var http_server = http.createServer(function(req, res) {
-
-  let filePath = "." + req.url;
-  if (filePath == './') filePath = './index.html';
-
-  fs.readFile(filePath, function(err, content){
-    if (err) {
-      console.log("ERROR: " + JSON.stringify(err)); //Report error to console
-      res.writeHead(404); //Respond with not found 404 to client
-      res.end(JSON.stringify(err));
-      return;
-    }
-    //Respond with file contents
-    res.writeHead(200, { "Content-Type": get_mime(filePath) })
-    res.end(content)
-  })
-
-
-
-
-
-
-  // //Handle URL parsing 
-  // let urlObj = url.parse(req.url, true, false)
-  // console.log("\n============================")
-  // console.log("PATHNAME: " + urlObj.pathname)
-  // console.log("REQUEST: " + ROOT_DIR + urlObj.pathname)
-  // console.log("METHOD: " + req.method)
-  // if (req.method === "POST") {
-  //   console.log("POST request not setup")
-  // }
-  // //Handle GET requests (e.g. queries for another page)
-  // if (req.method === "GET"){
-  //   fs.readFile(ROOT_DIR + urlObj.pathname, function(err, data) {
-  //     if (err) {
-  //       console.log("ERROR: " + JSON.stringify(err)) //Report error to console
-  //       res.writeHead(404) //Respond with not found 404 to client
-  //       res.end(JSON.stringify(err))
-  //       return
-  //     }
-  //     //Respond with file contents
-  //     res.writeHead(200, { "Content-Type": get_mime(urlObj.pathname) })
-  //     res.end(data)
-  //   })
-  // }
-
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
 });
- 
-let io = socketio(http_server);
+
+app.use(express.static(__dirname)); //current directory is root
+
 io.on('connection', function(socket) {
   socket.on('browser', function(data) {
     if(! remote_osc_ip) {
@@ -103,5 +39,7 @@ io.on('connection', function(socket) {
   });
 });
  
-http_server.listen(8080);
-console.log('Starting HTTP server on TCP port 8080');
+server.listen(PORT, err=>{ //app.listen(PORT) won't work
+  if (err) console.log(err)
+  else console.log(`=====Server listening on port ${PORT}======`)
+});
