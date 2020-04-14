73011889b3ba3adcdf0b0a71b67d25ddc45c8ca6
danielanapoli <dnapol01@gmail.com>
2020-04-14 11:18:34 -0400

    Cleaning up some duplication

--- trackpad/socketio/communication.js
+++ trackpad/socketio/communication.js
@@ -1,27 +1,30 @@
 var socket = io.connect();
 
 socket.on('osc', function (data) {
-    if (data.x == 1) {
-    console.log("HEAT ON");
-    }
-    else {
-    console.log("HEAT OFF");
-    }
+  if (data.x == 1) {
+    console.log("Heater ON")
+  } else {
+    console.log("Heater OFF")
+  }
 });
 
-function heatOn(risk){
-    console.log(`heatON() being called.`)   //troubleshooting
-    if(risk == 1){
-        socket.emit('browser', {x: 1 || 0}) //turn on heater
-    } 
-    else {
-        socket.emit('browser', {x: 2 || 0}) //turn off heater
-    }
+function heaterON() {
+  console.log("heaterON() called")
+  socket.emit('browser', {
+    x: 1 || 0
+  });
 }
 
+function heaterOFF() {
+  console.log("heaterOFF() called")
+  socket.emit('browser', {
+    x: 2 || 0
+  });
+}
+
+// disable subsequent clicks
 function linkDisable(link) {
-    console.log(`linkDisable() being called.`) //troubleshooting
-    link.onclick = function(event) {
-        event.preventDefault();
-    }
-} 
+ link.onclick = function(event) {
+    event.preventDefault();
+ }
+}  
